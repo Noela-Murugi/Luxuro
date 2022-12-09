d55e828222f9660060fd3fd63094f4f28c9fb898
Noela-Murugi <murugin254@gmail.com>
2022-12-09 14:42:13 +0300

    components update

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from "../nav/NavBar"
 import { Link } from "react-router-dom";
 import { Button } from "../styles";
+import '../Details.css'
 
 export default function JobDetails() {
 
@@ -10,22 +11,21 @@ export default function JobDetails() {
         <NavBar/>
 
         <div className='details'>
-            <h2>Station</h2>
-            <div className='job-details'>
-                <div className='job-details-image'>
-                </div>
+            {/* <h2>Station</h2> */}
                 <div className='job-details-content'>
-                    <h2>tt</h2>
-                    <h3>Job Requirement: </h3>
-                    <h3>Location:</h3>
-                    <h3>Salary: </h3>
-                    <h3>Application Deadline:</h3>
+                    <h3>Capacity: 20 - 50 people </h3>
+                    <h3>Features:<br/>
+                        * Flat screen Tv <br/>
+                        * A working area <br/>
+                        * Free Wi-Fi<br/>
+                        </h3>
+                    <h3>Location: Nairobi</h3>
                 </div>
             </div>
             <div className='action-buttons'>
             <Button as={Link} to="/new" >Reserve</Button>
             </div>
-        </div>
+        {/* </div> */}
         </>
   )
 }
